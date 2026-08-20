6be9a31e70e27bced40829a43dcbcfbbefb8d9e5
Jonathan Perry <jonperry113@gmail.com>
2026-08-19 20:19:47 -0700

    fix: get document details page working with async params

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useMutation, useQuery } from "convex/react";
 import dynamic from "next/dynamic";
-import { useMemo } from "react";
+import { use, useMemo } from "react";
 
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
@@ -11,26 +11,28 @@ import { Cover } from "@/components/cover";
 import { Toolbar } from "@/components/toolbar";
 
 interface DocumentIdPageProps {
-  params: {
+  params: Promise<{
     documentId: Id<"documents">;
-  };
+  }>;
 }
 
 export default function DocumentIdPage({ params }: DocumentIdPageProps) {
+  const { documentId } = use(params);
+
   const Editor = useMemo(
     () => dynamic(() => import("@/components/editor"), { ssr: false }),
     []
   );
 
   const document = useQuery(api.documents.getById, {
-    documentId: params.documentId,
+    documentId,
   });
 
   const update = useMutation(api.documents.update);
 
   const onChange = (content: string) => {
     update({
-      id: params.documentId,
+      id: documentId,
       content,
     });
   };
